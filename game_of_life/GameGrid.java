4762bc6858a1be0cd4e31578099273f2515df3b1
Isaiah <taumois@whs.school.nz>
2026-07-02 12:00:27 +1200

    Part Way Through

--- game_of_life/GameGrid.java
+++ game_of_life/GameGrid.java
@@ -5,39 +5,23 @@
 public interface GameGrid {
     
     /**
-     * A complete 2d array of this object's cell's states. The 1st dimension is row #; the 2nd dimension is column #
+     * A complete 2d array representive of this object's cell's states. The 1st dimension is row #; the 2nd dimension is column #
      * 
      * @return the 2d array of states
      */
     public Cell[][] cells();
     
     /**
-     * The value of a specified cell
+     * Change the value of a specified cell
      * 
      * @param column the column of the target cell
      * @param row    the row of the target cell
+     * @param value  the value to change to cell to
      */
-    public Cell cell(int column, int row);
+    public Cell setCell(int column, int row, Cell value);
     
     /**
-     * Sets the state of a specified cell within the buffer
-     * 
-     * @param column the column of the target cell
-     * @param row    the row of the target cell
-     * @param state  the state to set the target cell in the buffer
+     * Steps every cell forwards one generation using the rules of Life
      */
-    public void setBufferCell(int column, int row, Cell state);
-    
-    /**
-     * 'Pushes' the buffer onto the grid. I.e. Overrides the grid such that it is identicle to the buffer
-     */
-    public void pushBuffer();
-    
-    /**
-     * The number of alive neighbors the cell at the target position has in the non-buffer grid
-     * 
-     * @param column the column of the target cell
-     * @param row    the row of the target cell
-     */
-    public int neighborNumberOfCell(int column, int row);
+    public void step();
 }
